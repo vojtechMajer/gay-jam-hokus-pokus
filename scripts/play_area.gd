d49043489af7bc545fab8eff9266c187d2bfed09
Drevo23 <jan.rojicek2005@gmail.com>
2024-09-22 10:37:28 +0200

    ok

--- scripts/play_area.gd
+++ scripts/play_area.gd
@@ -1,31 +1,18 @@
 extends Node2D
 
-var layer=5
+var layer=0
 var layers=[]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	layers=[self.get_child(0),self.get_child(1),self.get_child(2),self.get_child(3),self.get_child(4),self.get_child(5)]
+	layers=[self.get_child(0)]
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var arr =empty_array_3(5,5,5)
-	var tile = $TileMapLayer6.local_to_map(get_global_mouse_position())
-	if (Input.is_action_just_released("scrollUp")):
-		if (layer!=1):
-			layers[layer].modulate.a8=10
-			layer-=1
-	if (Input.is_action_just_released("scrollDown")):
-		if (layer!=5):
-			layer+=1
-			layers[layer].modulate.a8=255
-	if(Input.is_action_pressed("rightClick")):
-		if (tile.x <= 30-layer)&&(tile.y <= 30-layer)&&(tile.x >= 0-layer)&&(tile.y >= 0-layer):
-			layers[layer].set_cell(tile,3,Vector2(0,0),0)
-	if(Input.is_action_pressed("leftClick")):
-		if (tile.x <= 30-layer)&&(tile.y <= 30-layer)&&(tile.x >= 0-layer)&&(tile.y >= 0-layer):
-			layers[layer].set_cell(tile,3,Vector2(0,1),0)
+	var tile = layers[0].local_to_map(get_global_mouse_position())
+	layers[0].set_cell(tile,0,Vector2(0,0),0)
+
 
 
 func empty_array_3(size_x: int, size_y: int, size_z: int, contents = 0) -> Array:
