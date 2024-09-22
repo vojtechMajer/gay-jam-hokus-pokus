aa3fea30bd7622941cf26e455d1bd6f171c2827a
Drevo23 <jan.rojicek2005@gmail.com>
2024-09-22 11:35:52 +0200

    comit

--- scripts/play_area.gd
+++ scripts/play_area.gd
@@ -1,32 +1,27 @@
 extends Node2D
-
-var layer=0
+const playSize=30
+var layer=4
 var layers=[]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	layers=[self.get_child(0)]
+	layers=[self.get_child(0),self.get_child(1),self.get_child(2),self.get_child(3),self.get_child(4),self.get_child(5)]
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var tile = layers[0].local_to_map(get_global_mouse_position())
-	layers[0].set_cell(tile,0,Vector2(0,0),0)
+	if (Input.is_action_just_released("scrollUp")):
+		if (layer!=1):
+			layers[layer].modulate.a8=50
+			layer-=1
+	if (Input.is_action_just_released("scrollDown")):
+		if (layer!=5):
+			layer+=1
+			layers[layer].modulate.a8=255
 
-
-
-func empty_array_3(size_x: int, size_y: int, size_z: int, contents = 0) -> Array:
-	var inner: = []
-	inner.resize(size_z)
-	inner.fill(contents)
-	
-	var mid: = []
-	mid.resize(size_y)
-	for y in size_y:
-		mid[y] = inner.duplicate()
-	
-	var outer: = []
-	outer.resize(size_x)
-	for x in size_x:
-		outer[x] = mid.duplicate()
-	return outer
+func _input(event: InputEvent):
+	var tile = layers[0].local_to_map(event.position)
+	if event.is_action_pressed("place_tile"):
+		if (tile.x <= playSize-layer)&&(tile.y <= playSize-layer)&&(tile.x >= 0-layer)&&(tile.y >= 0-layer):
+			layers[layer].set_cell(tile,0,Vector2(0,0),0)
