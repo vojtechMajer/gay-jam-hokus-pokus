76d428758895b3cbcf13d8bb2170560e412de472
Drevo23 <jan.rojicek2005@gmail.com>
2024-09-22 17:12:48 +0200

    placeholder cursor

--- scripts/play_area.gd
+++ scripts/play_area.gd
@@ -5,7 +5,7 @@ extends Node2D
 @export var layer_count := 4
 @export var tile_set_to_use: TileSet
 @export var ground_level_atlas_coords: Array[Vector2i]
-
+var placehorder_cursor:= [load("res://sprites/placeholder-tile.png"),load("res://sprites/placeholder-tile-mright.png"),load("res://sprites/placeholder-tile-mleft.png"),load("res://sprites/placeholder-tile-top.png")]
 var active_layer_id
 var layers: Array[TileMapLayer]
 @onready var placeholder_tile: Sprite2D = $PlaceholderTile
@@ -31,8 +31,6 @@ func _process(_delta: float) -> void:
 			active_layer_id += 1
 
 	## Move Placeholder Tile
-	var placeholder_pos := layers[active_layer_id].map_to_local(layers[active_layer_id].local_to_map(get_global_mouse_position())) #layers[0].local_to_map(get_global_mouse_position())
-	placeholder_tile.set_position(placeholder_pos)
 
 func spawn_layers():
 	for i in layer_count:
@@ -54,11 +52,24 @@ func fill_up(layer: TileMapLayer, start_pos: Vector2i, atlas_coord: Vector2i):
 
 ## Input Handling
 func _input(event: InputEvent):
+	##placeholder_cursor
+	cursor_state()
 	## Place Tile
 	if event.is_action_pressed("place_tile"):
 		## calculate tilemap position from mouse pos
 		var tile_pos = layers[0].local_to_map(get_global_mouse_position()) 
 		place_tile(tile_pos, Vector2i(0,0))
+
+func cursor_state():
+	var state=0
+	var placeholder_pos := layers[active_layer_id].map_to_local(layers[active_layer_id].local_to_map(get_global_mouse_position())) #layers[0].local_to_map(get_global_mouse_position())
+	placeholder_tile.set_position(placeholder_pos)
+	if(layers[active_layer_id].get_cell_atlas_coords(layers[active_layer_id].local_to_map(placeholder_pos)+Vector2i(1,0))!=Vector2i(-1,-1)):
+		state+=1
+	if(layers[active_layer_id].get_cell_atlas_coords(layers[active_layer_id].local_to_map(placeholder_pos)+Vector2i(0,1))!=Vector2i(-1,-1)):
+		state+=2
+	print(state)
+	placeholder_tile.texture =placehorder_cursor[state]
 
 func place_tile(tile_pos: Vector2i, atlas_coords: Vector2i):
 	if (tile_pos.x <= grid_width-1-active_layer_id)&&(tile_pos.y <= grid_height-1-active_layer_id)&&(tile_pos.x >= 0-active_layer_id)&&(tile_pos.y >= 0-active_layer_id):
