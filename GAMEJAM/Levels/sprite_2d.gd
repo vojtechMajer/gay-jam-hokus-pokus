55538ebdba4e16739dda9b2fc56bc1012aa38a86
Xander2662 <130229738+Xander2662@users.noreply.github.com>
2024-10-18 23:59:07 +0200

    meow
    meowmeow

--- GAMEJAM/Levels/sprite_2d.gd
+++ GAMEJAM/Levels/sprite_2d.gd
@@ -1,18 +1,39 @@
-extends Sprite2D
+extends Node2D
+
 var sizeOfLevel : Vector2i
 @export
 var allRuins: Array[Sprite2D]
+
+# Velikost pro náhodné umístění uvnitř kvadrantu
+const OFFSET = 64
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Velikost mapy
+	# Nastav velikost mapy
 	sizeOfLevel = Vector2i(47*32,32*32)
-	
+	createPositionOfRuins()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+# Vytvoření pozic ruin
 func createPositionOfRuins():
-	var kvadranty = [0,0,0,0]
-	
-	var random = randi_range(0,3)
+	# Definice kvadrantů
+	var quadrants = [
+		Rect2(Vector2(0, 0), sizeOfLevel / 2),  # horní levý
+		Rect2(Vector2(sizeOfLevel.x / 2, 0), sizeOfLevel / 2),  # horní pravý
+		Rect2(Vector2(0, sizeOfLevel.y / 2), sizeOfLevel / 2),  # dolní levý
+		Rect2(Vector2(sizeOfLevel.x / 2, sizeOfLevel.y / 2), sizeOfLevel / 2)  # dolní pravý
+	]
+
+	# Pro každý sprite v allRuins
+	for sprite in allRuins:
+		# Náhodně vyber kvadrant
+		var random_index = randi_range(0, 3)
+		var selected_quadrant = quadrants[random_index]
+
+		# Generuj náhodnou pozici uvnitř kvadrantu s offsetem
+		var random_position = Vector2(
+			randi_range(selected_quadrant.position.x + OFFSET, selected_quadrant.position.x + selected_quadrant.size.x - OFFSET),
+			randi_range(selected_quadrant.position.y + OFFSET, selected_quadrant.position.y + selected_quadrant.size.y - OFFSET)
+		)
+
+		# Nastav pozici sprite
+		sprite.position = random_position
