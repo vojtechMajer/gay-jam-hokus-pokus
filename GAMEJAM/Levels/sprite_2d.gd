22537e7cd6b96124969888bcd02a32cc51e2b52b
Xander2662 <130229738+Xander2662@users.noreply.github.com>
2024-10-18 22:08:17 +0200

    RuinCreation

---
+++ GAMEJAM/Levels/sprite_2d.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+var sizeOfLevel : Vector2i
+@export
+var allRuins: Array[Sprite2D]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#Velikost mapy
+	sizeOfLevel = Vector2i(47*32,32*32)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func createPositionOfRuins():
+	var kvadranty = [0,0,0,0]
+	var i = randi_range(0,3)
